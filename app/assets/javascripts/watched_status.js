4f677dde6125928a7c4f25aa00bf42b44e9e49dc
Ben Pepper <bfpepper87@gmail.com>
2017-03-09 13:13:16 -0700

    Started logic.

--- app/assets/javascripts/watched_status.js
+++ app/assets/javascripts/watched_status.js
@@ -1,6 +1,8 @@
 $(document).ready(function(){
   $('#movie-list').on('click', '.mark-watched', changeWatchedStatus)
   $('#movie-list').on('click', '.mark-unwatched', changeWatchedStatus)
+  $('.search-watched').on('click', getWatchedMovies)
+  $('.search-unwatched').on('click', getUnwatchedMovies)
 });
 
 function changeWatchedStatus() {
@@ -42,3 +44,56 @@ function changeDbStatus(movieId, currentStatus) {
     data: { watched: status },
   })
 };
+
+function getWatchedMovies() {
+  var movies = $.ajax({
+    url: "/api/v1/movies",
+    method: 'GET',
+    data: {status: true},
+  })
+
+  redrawPage(movies)
+};
+
+function getUnwatchedMovies() {
+  var movies = $.ajax({
+    url: "/api/v1/movies",
+    method: 'GET',
+    data: {status: false},
+  })
+
+  redrawPage(movies)
+};
+
+function redrawPage(movies) {
+//   debugger;
+//   $()
+//   '
+//   <table>
+//     <thead>
+//       <tr>
+//         <th>Title</th>
+//         <th>Note</th>
+//         <th>Watched?</th>
+//         <th>Edit</th>
+//         <th>Mark Watched Status</th>
+//       </tr>
+//     </thead>
+//     <tbody id="movie-list">
+//       <% if @movies %>
+//         <% @movies.each do |movie| %>
+//           <tr class=<%= color_output(movie.watched) %>><td><%= movie.title %></td>
+//           <td><%= movie.note %></td>
+//           <td id=<%= "watched-#{movie.id}" %>><%= movie.watched %></td>
+//           <td><%= link_to "Edit", edit_movie_path(movie) %></td>
+//           <% if movie.watched == false %>
+//             <td><button type="button" class="mark-watched" id=<%= "#{movie.id}" %>>Mark as Watched</button></td></tr>
+//           <% else %>
+//             <td><button type="button" class="mark-watched" id=<%= "#{movie.id}" %>>Mark as Unwatched</button></td></tr>
+//           <% end %>
+//         <% end %>
+//       <% end %>
+//     </tbody>
+//   </table>
+// '
+}
--- config/routes.rb
+++ config/routes.rb
@@ -8,7 +8,7 @@ Rails.application.routes.draw do
 
   namespace :api do
     namespace :v1 do
-      resources :movies, only: [:update]
+      resources :movies, only: [:update, :index]
     end
   end
 
