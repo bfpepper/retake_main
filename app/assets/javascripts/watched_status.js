4345bfb50c00e357b48409fa837c68f8ebcc81a2
Ben Pepper <bfpepper87@gmail.com>
2017-03-09 13:43:18 -0700

    Adds filter for watched/unwatched.

--- app/assets/javascripts/watched_status.js
+++ app/assets/javascripts/watched_status.js
@@ -1,10 +1,30 @@
 $(document).ready(function(){
   $('#movie-list').on('click', '.mark-watched', changeWatchedStatus)
-  $('#movie-list').on('click', '.mark-unwatched', changeWatchedStatus)
   $('.search-watched').on('click', getWatchedMovies)
   $('.search-unwatched').on('click', getUnwatchedMovies)
 });
 
+function getWatchedMovies() {
+  $('#movie-list').children().each(function() {
+    var readStatus = $(this).find(".mark-watched").html();
+    if (readStatus.includes("Mark as Unwatched")) {
+      $(this).show()
+    }  else {
+      $(this).hide()
+    };
+  });
+}
+
+function getUnwatchedMovies() {
+  $('#movie-list').children().each(function() {
+    var readStatus = $(this).find(".mark-watched").html();
+    if (readStatus.includes("Mark as Watched")) {
+      $(this).show()
+    }  else {
+      $(this).hide()
+    };
+  });
+}
 function changeWatchedStatus() {
   var movieId = $(this).parent().parent()[0].rowIndex
   var currentStatus = $( '#watched-' + movieId )[0].innerText
@@ -44,56 +64,3 @@ function changeDbStatus(movieId, currentStatus) {
     data: { watched: status },
   })
 };
-
-function getWatchedMovies() {
-  var movies = $.ajax({
-    url: "/api/v1/movies",
-    method: 'GET',
-    data: {status: true},
-  })
-
-  redrawPage(movies)
-};
-
-function getUnwatchedMovies() {
-  var movies = $.ajax({
-    url: "/api/v1/movies",
-    method: 'GET',
-    data: {status: false},
-  })
-
-  redrawPage(movies)
-};
-
-function redrawPage(movies) {
-//   debugger;
-//   $()
-//   '
-//   <table>
-//     <thead>
-//       <tr>
-//         <th>Title</th>
-//         <th>Note</th>
-//         <th>Watched?</th>
-//         <th>Edit</th>
-//         <th>Mark Watched Status</th>
-//       </tr>
-//     </thead>
-//     <tbody id="movie-list">
-//       <% if @movies %>
-//         <% @movies.each do |movie| %>
-//           <tr class=<%= color_output(movie.watched) %>><td><%= movie.title %></td>
-//           <td><%= movie.note %></td>
-//           <td id=<%= "watched-#{movie.id}" %>><%= movie.watched %></td>
-//           <td><%= link_to "Edit", edit_movie_path(movie) %></td>
-//           <% if movie.watched == false %>
-//             <td><button type="button" class="mark-watched" id=<%= "#{movie.id}" %>>Mark as Watched</button></td></tr>
-//           <% else %>
-//             <td><button type="button" class="mark-watched" id=<%= "#{movie.id}" %>>Mark as Unwatched</button></td></tr>
-//           <% end %>
-//         <% end %>
-//       <% end %>
-//     </tbody>
-//   </table>
-// '
-}
